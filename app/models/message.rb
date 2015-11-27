7d39a8cd4509c58c85cc70c97eb6d13bc0cf4e29
Satoshi Yamauchi <perfect.wanderer@gmail.com>
2015-11-27 13:10:59 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  body       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Message < ActiveRecord::Base
+  validates :name, length: {maximum: 20}, presence: true
+  validates :body, length: {minimum: 2, miximum: 50}, presence: true
 end
