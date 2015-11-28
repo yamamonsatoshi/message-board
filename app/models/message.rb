563e47f5dfee63109f6a236d4a6ceab7dbdadc23
Satoshi Yamauchi <perfect.wanderer@gmail.com>
2015-11-28 08:10:22 +0000

    modified label for bugs

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  body       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  age        :integer
+#
+
 class Message < ActiveRecord::Base
 end
