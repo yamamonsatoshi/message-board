0371e467c40e88ca694fde0cf1df04d5f60b2ceb
Satoshi Yamauchi <perfect.wanderer@gmail.com>
2015-11-28 03:21:41 +0000

    fixed app/view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_filter :message_params, :only => [:create]
+  before_action :set_message, only: [:edit, :update, :destroy]
   protect_from_forgery with: :null_session
   
   def index
@@ -7,10 +8,21 @@ class MessagesController < ApplicationController
      @message = Message.new
   end
   
+  def edit
+  end
+  
+  def update
+    if message.update(message_params)
+      redirect_to root_path, notice: '更新しました'
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path, flash[:notice] = 'メッセージを保存しました'
     else
       # メッセージが保存できなかった時
       @messages = Message.all
@@ -18,9 +30,18 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
